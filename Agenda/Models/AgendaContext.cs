dc5bc8e4d2cb1624508b956f2227c2210cdb013a
Pedro Jara <pedrojara2405@gmail.com>
2018-10-08 15:45:14 -0500

    Adding custom connection string

--- Agenda/Models/AgendaContext.cs
+++ Agenda/Models/AgendaContext.cs
@@ -4,6 +4,9 @@ namespace Agenda.Models
 {
     public class AgendaContext : DbContext
     {
+        public AgendaContext() : base("ConnAgendaDb")
+        {
+        }
         public DbSet<Contacto> Contactos { get; set; }
         public DbSet<Telefono> Telefonos { get; set; }
     }
