14b55449bd4376c94e8abea83b72af53b06f8f48
Pedro Jara <pedrojara2405@gmail.com>
2018-10-08 04:36:17 -0500

    Adding Contacto and Telefono models

---
+++ Agenda/Models/Contacto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Agenda.Models
+{
+    public class Contacto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public ICollection<Telefono> Telefonos { get; set; }
+    }
+}
---
+++ Agenda/Models/Telefono.cs
@@ -0,0 +1,10 @@
+﻿namespace Agenda.Models
+{
+    public class Telefono
+    {
+        public int Id { get; set; }
+        public string Numero { get; set; }
+        public string Nombre { get; set; }
+        public Contacto Contacto { get; set; }
+    }
+}
