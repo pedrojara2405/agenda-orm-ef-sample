82a6b793cda979a9883f7f8b1725271544cc8b9a
Pedro Jara <pedrojara2405@gmail.com>
2018-10-08 14:22:12 -0500

    Adding ContactoController and TelefonoController with their respective action views

---
+++ Agenda/Controllers/ContactoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Agenda.Models;
+
+namespace Agenda.Controllers
+{
+    public class ContactoController : Controller
+    {
+        private AgendaContext db = new AgendaContext();
+
+        // GET: Contacto
+        public ActionResult Index()
+        {
+            return View(db.Contactos.ToList());
+        }
+
+        // GET: Contacto/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contacto contacto = db.Contactos.Find(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
+
+        // GET: Contacto/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contacto/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombre")] Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contactos.Add(contacto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contacto);
+        }
+
+        // GET: Contacto/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contacto contacto = db.Contactos.Find(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
+
+        // POST: Contacto/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombre")] Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contacto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contacto);
+        }
+
+        // GET: Contacto/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contacto contacto = db.Contactos.Find(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
+
+        // POST: Contacto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contacto contacto = db.Contactos.Find(id);
+            db.Contactos.Remove(contacto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Agenda/Controllers/TelefonoController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Agenda.Models
+{
+    public class TelefonoController : Controller
+    {
+        private AgendaContext db = new AgendaContext();
+
+        // GET: Telefono
+        public ActionResult Index()
+        {
+            return View(db.Telefonos.ToList());
+        }
+
+        // GET: Telefono/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Telefono telefono = db.Telefonos.Find(id);
+            if (telefono == null)
+            {
+                return HttpNotFound();
+            }
+            return View(telefono);
+        }
+
+        // GET: Telefono/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Telefono/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Numero,Nombre")] Telefono telefono)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Telefonos.Add(telefono);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(telefono);
+        }
+
+        // GET: Telefono/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Telefono telefono = db.Telefonos.Find(id);
+            if (telefono == null)
+            {
+                return HttpNotFound();
+            }
+            return View(telefono);
+        }
+
+        // POST: Telefono/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Numero,Nombre")] Telefono telefono)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(telefono).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(telefono);
+        }
+
+        // GET: Telefono/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Telefono telefono = db.Telefonos.Find(id);
+            if (telefono == null)
+            {
+                return HttpNotFound();
+            }
+            return View(telefono);
+        }
+
+        // POST: Telefono/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Telefono telefono = db.Telefonos.Find(id);
+            db.Telefonos.Remove(telefono);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
