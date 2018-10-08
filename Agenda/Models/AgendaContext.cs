cf7b5358512d92eccb646b209fdd0012d8de2527
Pedro Jara <pedrojara2405@gmail.com>
2018-10-08 04:44:48 -0500

    Adding AgendaContext

---
+++ Agenda/Models/AgendaContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace Agenda.Models
+{
+    public class AgendaContext : DbContext
+    {
+        public DbSet<Contacto> Contactos { get; set; }
+        public DbSet<Telefono> Telefonos { get; set; }
+    }
+}
